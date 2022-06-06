8eef0b76043207c9ffd423f51eece2dca582b655
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-06-06 13:58:28 +0700

    Add latest listing IDs to UI

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
-    return <div>Test auto-deploy</div>;
+    const [listings, setListings] = useState([]);
+
+    useEffect(() => {
+        const url = '/api/cryptocurrency/listings/latest';
+        axios.get(url).then((response) => {
+            setListings(response.data.data);
+        });
+    }, []);
+
+    return listings.map((listing) => <div key={listing.id}>{listing.id}</div>);
 }
