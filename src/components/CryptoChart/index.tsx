f11e7b295aee13d44d8c666b93ddb099a7efac66
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-14 17:00:58 +0700

    Extract Candlestick component into separate file

--- src/components/CryptoChart/index.tsx
+++ src/components/CryptoChart/index.tsx
@@ -1,75 +1,9 @@
 import { PuffLoader } from 'react-spinners';
 import { XAxis, YAxis, ResponsiveContainer, Tooltip, BarChart, Bar, Cell } from 'recharts';
 import { useEffect, useState } from 'react';
-import cn from 'classnames';
 import { currencyFormatter, dateFormatter } from '../../utils';
 import CustomTooltip from '../CustomTooltip';
-
-const Candlestick = (props: any) => {
-    const {
-        x,
-        y,
-        width,
-        height,
-        low,
-        high,
-        openClose: [open, close],
-        showHighLow,
-    } = props;
-    const isGrowing = open < close;
-    const color = isGrowing ? 'green' : 'red';
-    const ratio = Math.abs(height / (open - close));
-
-    return (
-        <g stroke={color} fill='black' strokeWidth='2'>
-            <path
-                d={`
-          M ${x},${y}
-          L ${x},${y + height}
-          L ${x + width},${y + height}
-          L ${x + width},${y}
-          L ${x},${y}
-        `}
-            />
-            {/* bottom line */}
-            {isGrowing ? (
-                <path
-                    className={cn({ 'opacity-0': !showHighLow })}
-                    d={`
-            M ${x + width / 2}, ${y + height}
-            v ${(open - low) * ratio}
-          `}
-                />
-            ) : (
-                <path
-                    className={cn({ 'opacity-0': !showHighLow })}
-                    d={`
-            M ${x + width / 2}, ${y}
-            v ${(close - low) * ratio}
-          `}
-                />
-            )}
-            {/* top line */}
-            {isGrowing ? (
-                <path
-                    className={cn({ 'opacity-0': !showHighLow })}
-                    d={`
-            M ${x + width / 2}, ${y}
-            v ${(close - high) * ratio}
-          `}
-                />
-            ) : (
-                <path
-                    className={cn({ 'opacity-0': !showHighLow })}
-                    d={`
-            M ${x + width / 2}, ${y + height}
-            v ${(open - high) * ratio}
-          `}
-                />
-            )}
-        </g>
-    );
-};
+import Candlestick from '../Candlestick';
 
 const prepareData = (data: any) => {
     if (!data) return [];
