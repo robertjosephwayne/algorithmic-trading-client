4da8498bf03f290693f1fc61a2dc92c4a12e3d0e
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-15 17:22:03 +0700

    Create initial version of position summary table

---
+++ src/routes/Positions.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useGetAccountQuery, useGetPositionsQuery } from '../api/apiSlice';
+import Loader from '../components/Loader';
+import Page from '../components/Page';
+import PositionSummaryTable from '../components/PositionSummaryTable';
+import { currencyFormatter } from '../utils';
+
+export default function Positions() {
+    const { data: accountData, isLoading: accountQueryIsLoading } = useGetAccountQuery({});
+
+    return (
+        <Page>
+            {accountQueryIsLoading ? (
+                <Loader fullPage={true} />
+            ) : (
+                <div className='p-4'>
+                    <span className='font-bold'>Account Summary - Paper Trading</span>
+                    <div className='flex flex-col pt-4'>
+                        <span>Buying Power: {currencyFormatter(accountData.buying_power)}</span>
+                        <span>Cash: {currencyFormatter(accountData.cash)}</span>
+                        <span>
+                            Position Market Value:{' '}
+                            {currencyFormatter(accountData.position_market_value)}
+                        </span>
+                        <span>Equity: {currencyFormatter(accountData.equity)}</span>
+                    </div>
+                    <div className='mt-6'>
+                        <span className='font-bold'>Positions</span>
+                        <PositionSummaryTable />
+                    </div>
+                </div>
+            )}
+        </Page>
+    );
+}
