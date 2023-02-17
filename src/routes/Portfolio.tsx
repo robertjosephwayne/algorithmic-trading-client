58b2a159ae2b31e3b6c7211fd81d4e0f4f182bb4
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-02-17 15:34:46 +0700

    Remove unused variables

--- src/routes/Portfolio.tsx
+++ src/routes/Portfolio.tsx
@@ -1,38 +1,21 @@
 import { formatRFC3339 } from 'date-fns';
 import { useState } from 'react';
 import { useGetPortfolioHistoryQuery } from '../api/apiSlice';
-import PortfolioChartControlPanel from '../components/PortfolioChartControlPanel';
 import Page from '../components/Page';
 import PortfolioChart from '../components/PortfolioChart';
 
 export default function Portfolio() {
-    const [selectedTimeframe, setSelectedTimeframe] = useState('1D');
-    const [startDate, setStartDate] = useState(formatRFC3339(new Date(2023, 1, 1)));
+    const [selectedTimeframe] = useState('1D');
+    const [startDate] = useState(formatRFC3339(new Date(2023, 1, 1)));
 
     const { data, isLoading } = useGetPortfolioHistoryQuery({
         timeframe: selectedTimeframe,
         start: startDate,
     });
 
-    const handleStartDateChange = (startDate: string | null) => {
-        if (startDate) {
-            setStartDate(formatRFC3339(new Date(startDate)));
-        }
-    };
-
-    const handleTimeframeChange = (timeframe: string) => {
-        setSelectedTimeframe(timeframe);
-    };
-
     return (
         <Page>
             <PortfolioChart data={data} isLoading={isLoading} />
-            <PortfolioChartControlPanel
-                selectedTimeframe={selectedTimeframe}
-                startDate={startDate}
-                onTimeframeChange={handleTimeframeChange}
-                onStartDateChange={handleStartDateChange}
-            />
         </Page>
     );
 }
