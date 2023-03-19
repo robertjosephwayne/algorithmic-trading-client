62820c7c18e086b4e227f3adc443b8b35eaab9a7
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-03-19 22:13:22 +0700

    Add all pages to sidebar

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -2,19 +2,16 @@ import { useGetOrdersQuery } from '../api/apiSlice';
 import Loader from '../components/Loader';
 import Page from '../components/Page';
 import OrderSummaryTable from '../components/OrderSummaryTable';
+import pageRoutes from '../routes';
+import Sidenav from '../components/Sidenav';
 
 export default function Orders() {
     const { isLoading: ordersQueryIsLoading } = useGetOrdersQuery({});
 
     return (
         <Page>
-            {ordersQueryIsLoading ? (
-                <Loader fullPage={true} />
-            ) : (
-                <div className='w-full h-4/5'>
-                    <OrderSummaryTable />
-                </div>
-            )}
+            <Sidenav routes={pageRoutes} brandName='Trading Dashboard' />
+            <OrderSummaryTable />
         </Page>
     );
 }
