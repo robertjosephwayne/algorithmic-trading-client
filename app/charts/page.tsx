2c443541db940660c13351c1bf191a665ff22094
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-04-05 16:29:54 +0700

    Add get positions query to charts page

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
+import { useGetPositionsQuery } from '../../api/apiSlice';
+import Loader from '../../components/Loader';
 
 export default function Charts() {
+    const { isLoading: positionsQueryIsLoading } = useGetPositionsQuery({});
+
     return (
         <div className='w-full h-4/5'>
-            <AdvancedRealTimeChart theme='dark' autosize={true} interval='W' />
+            {positionsQueryIsLoading ? (
+                <Loader fullPage={true} />
+            ) : (
+                <AdvancedRealTimeChart theme='dark' autosize={true} interval='W' />
+            )}
         </div>
     );
 }
