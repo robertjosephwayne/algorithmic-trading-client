4714931131e77ef4ce52751a3719e985e9c2ee2e
Robert Joseph Wayne <robertjwayne@gmail.com>
2023-04-05 16:25:15 +0700

    Change chart interval to week

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -5,7 +5,7 @@ import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
 export default function Charts() {
     return (
         <div className='w-full h-4/5'>
-            <AdvancedRealTimeChart theme='dark' autosize={true} interval='D' />
+            <AdvancedRealTimeChart theme='dark' autosize={true} interval='W' />
         </div>
     );
 }
